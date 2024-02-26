1c5ac55443f600666ab00d173bd90bf071013b45
suresh298877 <sureshpeddimsetti298@gmai.com>
2024-02-26 18:48:50 +0530

    asdlkfj

--- Expense_Tracker_App_Practice/screens/ManageExpense.js
+++ Expense_Tracker_App_Practice/screens/ManageExpense.js
@@ -1,16 +1,74 @@
-import { Text } from "react-native";
 import { useLayoutEffect } from "react";
+import { StyleSheet, View } from "react-native";
+import Button from "../UI/Button";
+import IconButton from "../UI/IconButton";
+import { GlobalStyles } from "../constants/styles";
 function ManageExpense({ route, navigation }) {
-    
+
     const editedExpenseId = route.params?.expenseId;
-    const idEditing = !!editedExpenseId;
+    const isEditing = !!editedExpenseId;
 
     useLayoutEffect(() => {
         navigation.setOptions({
-            title: idEditing ? 'Edit Expense' : 'Add Expense'
-        }, [navigation, idEditing]);
+            title: isEditing ? 'Edit Expense' : 'Add Expense'
+        }, [navigation, isEditing]);
     });
-    return <Text>RecentExpenses</Text>
+
+    function deleteExpenseHandler() {
+        navigation.goBack();
+    }
+
+    function cancelHandler(){
+        navigation.goBack();
+    }
+
+    function confirmHandler(){
+        navigation.goBack();
+    }
+    return (
+        <View style={styles.container}>
+            <View style={styles.buttons}>
+                <Button style={styles.button} mode="flat" onPress={cancelHandler}>
+                    Cancel
+                </Button>
+                <Button style={styles.button} onPress={confirmHandler}>{isEditing ? 'Update' : 'Add'}</Button>
+            </View>
+            {isEditing && (
+                <View style={styles.deleteContainer}>
+                    <IconButton
+                        icon="trash"
+                        color={GlobalStyles.colors.primary500}
+                        size={36}
+                        onPress={deleteExpenseHandler}
+                    />
+                </View>
+            )}
+        </View>
+    )
 }
 
 export default ManageExpense;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 24,
+        backgroundColor: GlobalStyles.colors.primary800,
+    },
+    buttons : {
+        flexDirection : 'row',
+        justifyContent : 'center',
+        alignItems : 'center'
+    },
+    button : {
+        minWidth : 120,
+        marginHorizontal : 8,
+    },
+    deleteContainer: {
+        marginTop: 16,
+        paddingTop: 8,
+        borderTopWidth: 2,
+        borderTopColor: GlobalStyles.colors.primary200,
+        alignItems: 'center',
+    },
+});
