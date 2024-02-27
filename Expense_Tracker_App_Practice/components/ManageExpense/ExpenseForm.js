a55ccf0c38a4f489b9dce228250812b381e5a709
suresh298877 <sureshpeddimsetti298@gmail.com>
2024-02-27 20:06:48 +0530

    adsflkjk

--- Expense_Tracker_App_Practice/components/ManageExpense/ExpenseForm.js
+++ Expense_Tracker_App_Practice/components/ManageExpense/ExpenseForm.js
@@ -1,29 +1,57 @@
-import { View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 import Input from "./Input";
 
 function ExpenseForm() {
 
-    function amountChangeHandler(){
+    function amountChangeHandler() {
 
     }
-    return <View>
-        <Input
-            label="Amount"
-            textInputConfig={{
-                keyboardType : 'decimal-pad',
-                onChangeText : amountChangeHandler,
-            }}
-        />
-        <Input label="Date" textInputConfig={{
-            placeholder : 'YYYY-MM-DD',
-            maxLength : 10,
-            onChangeText : ()=>{},
-        }} />
+    return <View style={styles.form}>
+        <Text style={styles.title}>Your Expense</Text>
+        <View style={styles.inputRow}>
+            <Input
+                style={styles.rowInput}
+                label="Amount"
+                textInputConfig={{
+                    keyboardType: 'decimal-pad',
+                    onChangeText: amountChangeHandler,
+                }}
+            />
+            <Input
+                style={styles.rowInput}
+                label="Date"
+                textInputConfig={{
+                    placeholder: 'YYYY-MM-DD',
+                    maxLength: 10,
+                    onChangeText: () => { },
+                }} />
+        </View>
         <Input label="Description" textInputConfig={{
-            multiline : true,
-            }}
-         />
+            multiline: true,
+        }}
+        />
     </View>
 }
 
 export default ExpenseForm;
+
+const styles = StyleSheet.create({
+    inputRow: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+    },
+    rowInput: {
+        flex: 1,
+    },
+    form:{
+        marginTop : 40,
+        marginBottom : 20,
+    },
+    title : {
+        fontSize : 24,
+        fontWeight : 'bold',
+        color : 'white',
+        textAlign : 'center',
+
+    }
+})
--- Expense_Tracker_App_Practice/components/ManageExpense/Input.js
+++ Expense_Tracker_App_Practice/components/ManageExpense/Input.js
@@ -1,11 +1,17 @@
 import { StyleSheet, Text, TextInput, View } from "react-native";
 import { GlobalStyles } from "../../constants/styles";
 
-function Input({label,textInputConfig}){
+function Input({label,style,textInputConfig}){
+
+    const inputStyles=[styles.input]
+
+    if(textInputConfig && textInputConfig.multiline){
+        inputStyles.push(styles.inputMultiline);
+    }
     return (
-        <View style={styles.inputContainer}>
+        <View style={[styles.inputContainer,style]}>
             <Text style={styles.label}>{label}</Text>
-            <TextInput style={styles.input} {...textInputConfig} />
+            <TextInput style={inputStyles} {...textInputConfig} />
         </View>
     );
 }
@@ -28,5 +34,9 @@ const styles=StyleSheet.create({
         padding : 6,
         borderRadius : 6,
         fontSize : 18,
+    },
+    inputMultiline : {
+        minHeight : 100,
+        textAlignVertical : 'top',
     }
 })
