2b3b23c260433c4ca1011fd8b47aaeb104a7b900
suresh298877 <sureshpeddimsetti298@gmail.com>
2024-02-29 10:11:07 +0530

    adfsl

--- Expense_Tracker_App_Practice/util/http.js
+++ Expense_Tracker_App_Practice/util/http.js
@@ -1,7 +1,15 @@
 import axios from "axios";
 
+
+const BACKEND_URL='https://react-native-course-bfc44-default-rtdb.firebaseio.com';
 export function storeExpense(expenseData){
-    axios.post('https://react-native-course-bfc44-default-rtdb.firebaseio.com/expenses.json',
+    axios.post(BACKEND_URL+'/expenses.json',
     expenseData
     );
 }
+
+export function fetchExpenses(){
+    axios.get(
+        BACKEND_URL+'/expenses.json'
+    );
+};
